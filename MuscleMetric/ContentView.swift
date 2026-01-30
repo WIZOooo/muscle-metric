0fc9f44e4690ed6c70a7af49cbfb9034186c2a8a
zhouwei <alb_zzz@163.com>
2026-01-30 17:26:16 +0800

    Initial Commit

---
+++ MuscleMetric/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuscleMetric
+//
+//  Created by iMac on 2026/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuscleMetric/MuscleMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMetricApp.swift
+//  MuscleMetric
+//
+//  Created by iMac on 2026/1/30.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
